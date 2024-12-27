bfc74e670e7fed111dd74843516955715dd689d6
alex <alexmal24122004@gmail.com>
2024-12-27 23:31:56 +0300

    project structure refactoring

--- pachca-api-open-api-3-0-client/pachca_api_open_api_3_0_client/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing PachcaAPI - OpenAPI 3.0"""
-
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
---
+++ src/generator1/pachca.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from pachca_api_open_api_3_0_client.client import Pachca
+from pachca_api_open_api_3_0_client.models.create_chat_body import (
+    CreateChatBody,
+)
+from pachca_api_open_api_3_0_client.models.query_chat import QueryChat
+
+query_chat = QueryChat(name='test')
+chat_body = CreateChatBody(chat=query_chat)
+
+
+pachca = Pachca(
+    token='x4EhHyzYY2aA38GJb6AnKQXcY716LnEHCoxD1dUEyCI',
+)
+
+
+async def main() -> None:
+    """ Функция теста эндпоинтов """
+
+    task1 = asyncio.create_task(pachca.createChat(body=chat_body))
+    task2 = asyncio.create_task(pachca.getEmployees())
+
+    print(await task1)
+    print('*' * 30)
+    print(await task2)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
