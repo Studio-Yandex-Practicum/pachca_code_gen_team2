7e86daa4e7f8030a805fe74f1282a8474f9c4812
k0sdm1 <dima.corsin@yandex.ru>
2024-12-29 16:46:45 +0300

    add enum support for pydantic models

--- src/generator2/constants.py
+++ src/generator2/constants.py
@@ -1,10 +1,18 @@
-PATH_TO_YAML = './openapi.yaml'
+import sys
+
+PATH_TO_YAML = './src/generator2/openapi.yaml'
 
 PYTHON_TYPES = {
     'string': 'str',
     'integer': 'int',
     'boolean': 'bool',
 }
+
+ENUM_TYPES = {
+    'string': 'str, Enum' if sys.version_info[1] < 11 else 'StrEnum',
+    'integer': 'IntEnum',
+}
+
 HTTP_METHODS = (
     'get', 'post', 'put', 'update', 'patch', 'delete',
 )
