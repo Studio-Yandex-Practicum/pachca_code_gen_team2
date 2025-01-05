27ffd7bfe6116baa5d6023b303bff083c769a44a
SanyM2007 <sanym2007@yandex.ru>
2025-01-06 01:49:54 +0500

    library_sanym_ver1

--- src/generator1/pachca.py
+++ src/generator1/pachca.py
@@ -4,9 +4,9 @@ from pachca_api_open_api_3_0_client.client import Pachca
 from pachca_api_open_api_3_0_client.models.create_chat_body import (
     CreateChatBody,
 )
-from pachca_api_open_api_3_0_client.models.query_chat import QueryChat
+from pachca_api_open_api_3_0_client.models.chat import Chat
 
-query_chat = QueryChat(name='test')
+query_chat = Chat(name='test')
 chat_body = CreateChatBody(chat=query_chat)
 
 
