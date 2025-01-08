387efb8dbe4988df2a7136353f9c2eb5a602307d
Даниил Колчак <daniil.ko1chak@yandex.ru>
2025-01-08 17:12:29 +0300

    Fix bug getTag 404, createMessage 404, deleteMessageReactions 404, editMessage 200

--- src/generator1/pachca.py
+++ src/generator1/pachca.py
@@ -5,24 +5,48 @@ from pachca_api_open_api_3_0_client.models.create_chat_body import (
     CreateChatBody,
 )
 from pachca_api_open_api_3_0_client.models.query_chat import QueryChat
+from pachca_api_open_api_3_0_client.models.edit_messages import EditMessages
+from pachca_api_open_api_3_0_client.models.create_message import (
+    CreateMessage)
+from pachca_api_open_api_3_0_client.models.create_message_body import (
+    CreateMessageBody)
+from pachca_api_open_api_3_0_client.models.edit_message_body import (
+    EditMessageBody)
 
-query_chat = QueryChat(name='test')
+query_chat = QueryChat(name='Testing')
+
 chat_body = CreateChatBody(chat=query_chat)
 
+create_meassage = CreateMessage(
+    entity_id=1781540, content='NOT SUPER PUPER2222')
+
+edit_meassage = EditMessages(content='NOT and NOT SUPER PUPER')
+
+edit_message_body = EditMessageBody(message=edit_meassage)
+
+message_body = CreateMessageBody(message=create_meassage)
 
 pachca = Pachca(
-    token='x4EhHyzYY2aA38GJb6AnKQXcY716LnEHCoxD1dUEyCI',
+    token='qW3V2Kw7yxu1UA5OZLCdyoyKFfWA6OYr_MK2WR6PxbA',
 )
 
 
 async def main() -> None:
     """Функция теста эндпоинтов"""
-    task1 = asyncio.create_task(pachca.createChat(body=chat_body))
-    task2 = asyncio.create_task(pachca.getEmployees())
-
+    task1 = asyncio.create_task(pachca.createMessage(body=message_body))
+    task2 = asyncio.create_task(pachca.getTag(2232323))
+    task3 = asyncio.create_task(pachca.deleteMessageReactions(
+        id=412338865, code='😭'))
+    task4 = asyncio.create_task(pachca.editMessage(
+        id=412502100, body=edit_message_body))
     print(await task1)
     print('*' * 30)
     print(await task2)
+    print('*' * 30)
+    print(await task3)
+    print('*' * 30)
+    print(await task4)
+
 
 
 if __name__ == '__main__':
