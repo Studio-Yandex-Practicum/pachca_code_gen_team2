cca1c368a026939addf7dc27a82ca13b4f643c91
alex <alexmal24122004@gmail.com>
2025-01-08 22:12:56 +0300

    sorted test-requests

--- src/generator1/pachca.py
+++ src/generator1/pachca.py
@@ -5,25 +5,30 @@ from pachca_api_open_api_3_0_client.client import Pachca
 from pachca_api_open_api_3_0_client.models.create_chat_body import (
     CreateChatBody,
 )
-from pachca_api_open_api_3_0_client.models.create_message import (
-    CreateMessage)
+from pachca_api_open_api_3_0_client.models.create_message import CreateMessage
 from pachca_api_open_api_3_0_client.models.create_message_body import (
-    CreateMessageBody)
+    CreateMessageBody,
+)
 from pachca_api_open_api_3_0_client.models.create_task_body import (
-    CreateTaskBody)
+    CreateTaskBody,
+)
 from pachca_api_open_api_3_0_client.models.create_task_body_task import (
-    CreateTaskBodyTask)
+    CreateTaskBodyTask,
+)
 from pachca_api_open_api_3_0_client.models.edit_message_body import (
-    EditMessageBody)
+    EditMessageBody,
+)
 from pachca_api_open_api_3_0_client.models.edit_messages import EditMessages
 from pachca_api_open_api_3_0_client.models.get_message_reactions_body import (
-    GetMessageReactionsBody)
-from pachca_api_open_api_3_0_client.models.post_message_reactions_body import (
-    PostMessageReactionsBody)
-from pachca_api_open_api_3_0_client.models.query_chat import QueryChat
+    GetMessageReactionsBody,
+)
 from pachca_api_open_api_3_0_client.models.post_members_to_chats_body import (
     PostMembersToChatsBody,
 )
+from pachca_api_open_api_3_0_client.models.post_message_reactions_body import (
+    PostMessageReactionsBody,
+)
+from pachca_api_open_api_3_0_client.models.query_chat import QueryChat
 
 query_chat = QueryChat(name='Testing')
 
@@ -32,7 +37,6 @@ members_body = PostMembersToChatsBody(member_ids=[516675])
 members_body = PostMembersToChatsBody(member_ids=[516682])
 create_meassage = CreateMessage(
     entity_id=17802862, content='NOT SUPER PUPER2222')
-# edit_meassage = EditMessages(content='NOT and NOT SUPER PUPER')
 edit_meassage = EditMessages(content='Вот так  вот!')
 message_body = CreateMessageBody(message=create_meassage)
 edit_message_body = EditMessageBody(message=edit_meassage)
@@ -63,33 +67,27 @@ pachca = Pachca(
 
 async def main() -> None:
     """ Функция теста эндпоинтов """
-
-    task6 = asyncio.create_task(pachca.postMembersToChats(
-       id=999, body=members_body))
-    task5 = asyncio.create_task(pachca.leaveChat(id=111))
-    task7 = asyncio.create_task(pachca.createThread(id=412338865))
-    # task8 = asyncio.create_task(pachca.getListMessage(chat_id=17802862))
-    # task9 = asyncio.create_task(pachca.getMessage(id=412338865))
-    # task10 = asyncio.create_task(pachca.editMessage(
-    #    id=412338865, body=edit_message_body)
-    # )
-    # task11 = asyncio.create_task(pachca.postMessageReactions(
-    #    id=412338865, body=post_reactions)
-    # )
-    # task12 = asyncio.create_task(pachca.deleteMessageReactions(
-    #    id=412338865, code='😭')
-    # )
-    # task13 = asyncio.create_task(pachca.getMessageReactions(
-    #    id=412338865, body=reaction_body)
-    # )
-    # task14 = asyncio.create_task(pachca.createTask(body=body_task))
-    """Функция теста эндпоинтов"""
     task1 = asyncio.create_task(pachca.createMessage(body=message_body))
     task2 = asyncio.create_task(pachca.getTag(2232323))
-    task3 = asyncio.create_task(pachca.deleteMessageReactions(
-        id=412338865, code='😭'))
-    task4 = asyncio.create_task(pachca.editMessage(
-        id=412502100, body=edit_message_body))
+    task3 = asyncio.create_task(pachca.leaveChat(id=111))
+    task4 = asyncio.create_task(pachca.postMembersToChats(
+       id=999, body=members_body))
+    task5 = asyncio.create_task(pachca.createThread(id=412338865))
+    task6 = asyncio.create_task(pachca.getListMessage(chat_id=17802862))
+    task7 = asyncio.create_task(pachca.getMessage(id=412338865))
+    task8 = asyncio.create_task(pachca.editMessage(
+       id=412338865, body=edit_message_body)
+    )
+    task9 = asyncio.create_task(pachca.postMessageReactions(
+       id=412338865, body=post_reactions)
+    )
+    task10 = asyncio.create_task(pachca.deleteMessageReactions(
+       id=412338865, code='😭')
+    )
+    task11 = asyncio.create_task(pachca.getMessageReactions(
+       id=412338865, body=reaction_body)
+    )
+    task12 = asyncio.create_task(pachca.createTask(body=body_task))
     print(await task1)
     print('*' * 30)
     print(await task2)
@@ -103,7 +101,17 @@ async def main() -> None:
     print(await task6)
     print('*' * 30)
     print(await task7)
-    # print('*' * 30)
+    print('*' * 30)
+    print(await task8)
+    print('*' * 30)
+    print(await task9)
+    print('*' * 30)
+    print(await task10)
+    print('*' * 30)
+    print(await task11)
+    print('*' * 30)
+    print(await task12)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
