3c49ca7b81b002cc97d264b338bcfaf72a427bf2
k0sdm1 <dmitrykostin968@gmail.com>
2025-01-09 16:25:07 +0300

    testing api with request sequence

--- src/generator2/pachca.py
+++ src/generator2/pachca.py
@@ -1,13 +1,21 @@
 import asyncio
 
 from bot import Bot
+from models.models_reqBod_putStatus import Putstatus
+from models.models_reqBod_createChat import Createchat
+from models.models_reqBod_createTask import Createtask
+from models.models_reqBod_postMessageReactions import Postmessagereactions
+from models.models_reqBod_editMessage import Editmessage
+from models.models_reqBod_postTagsToChats import Posttagstochats
 from models.models_reqBod_createMessage import Createmessage, Message
+from models.models_reqBod_postMembersToChats import Postmemberstochats
 
 if __name__ == '__main__':
 
     print(id(Bot))
     print(Bot)
-    pachca = Bot(token='Bearer 35KekGygDNiFwtPpqUe44CaEZ_EVL17ycYRJrMnvHOs')
+    # pachca = Bot(token='Bearer 35KekGygDNiFwtPpqUe44CaEZ_EVL17ycYRJrMnvHOs')
+    pachca = Bot(token='Bearer Qm5KCvyTwI5v0ZthjSQUVDxyvXEjieaCLJmMaFLYZ38')
 
     print(pachca.token)
     print(hasattr(pachca, 'get_common_methods'))
@@ -19,18 +27,157 @@ if __name__ == '__main__':
     ))
     #print(message_test.model_dump())
     async def run_pachca():
-        print(await pachca.get_employee(id=514505))
-        print(await pachca.get_employees())
-        print(await pachca.get_tags())
-        print(await pachca.get_tags_employees(id=27470))
-        print(await pachca.get_chats(per=2))
-        message = await pachca.create_message(data=message_test)
-        print(message)
+        # print(await pachca.get_employee(id=515190))
+        # print(await pachca.get_employees())
+        # print(await pachca.get_chats(per=2))
+        # message = await pachca.create_message(data=message_test)
+        # print(message)
         #print(await pachca.get_common_methods()) #Возвращает ошибку, нужно прописать обработку если (parameters.query и parameters.required)
+
+        created_chat = await pachca.create_chat(
+            data=Createchat(
+                chat={
+                    'name': 'Тестовая беседа 31',
+                    'channel': False,
+                    'public': True
+                }
+            )
+        )
+        print('create_chat', created_chat, '\n','*'*60)
+        all_chats = await pachca.get_chats()
+        print('get_chats', all_chats, '\n','*'*60)
+        chat = await pachca.get_chat(created_chat.data.id)
+        print('get_chat', chat, '\n','*'*60)
+
+        response_post_members = await pachca.post_members_to_chats(
+            id=chat.data.id,
+            data=Postmemberstochats(
+                member_ids=[515190],
+                silent=False
+            )
+        )
+        print('post_members_to_chats', response_post_members, '\n','*'*60)
+
+        response_create_message = await pachca.create_message(
+            data=Createmessage(
+                message={
+                    'content': f'Запощеное сообщение в беседу {chat.data.id}',
+                    'entity_type': 'discussion',
+                    'entity_id': chat.data.id
+                }
+            )
+        )
+        print('create_message', response_create_message, '\n','*'*60)
+
+        #cant test without valid tags
+        # response = await pachca.post_tags_to_chats(
+        #     id=17519775,
+        #     data=Posttagstochats(
+        #         group_tag_ids=[1, 2, 3]
+        #     )
+        # )
+        print(await pachca.get_tags())
+        # print(await pachca.get_tags_employees()
+        #done:
+        response_create_thread = await pachca.create_thread(
+            id=response_create_message.data.id
+        )
+        print('create_thread', response_create_thread, '\n','*'*60)
+        #done
+        response_create_message_in_thread = await pachca.create_message(
+            data=Createmessage(
+                message={
+                    'content': f'Новое сообщение в тред {response_create_thread.data.id}',
+                    'entity_type': 'thread',
+                    'entity_id': response_create_thread.data.id
+                }
+            )
+        )
+        print('create_message_in_thread', response_create_message_in_thread, '\n','*'*60)
+        # done
+        response_list_messages = await pachca.get_list_message(
+            chat_id=response_create_thread.data.chat_id, per=10, page=1
+        )
+        print('get_list_message', response_list_messages, '\n','*'*60)
+        #done
+        response_get_message = await pachca.get_message(
+            id=response_create_message_in_thread.data.id
+        )
+        print('get_message', response_get_message, '\n','*'*60)
+        response_edit_message = await pachca.edit_message(
+            id=response_create_message_in_thread.data.id,
+            data=Editmessage(message={
+                'content': ('РЕДАКТИРОВАНИЕ СООБЩЕНИЯ '
+                            f'{response_create_message_in_thread.data.id} '
+                            'ЧЕРЕЗ АПИ СОВЕРШЕНО УСПЕШНО')
+            })
+        )
+        print('edit_message', response_edit_message, '\n','*'*60)
+        response_add_reaction = await pachca.post_message_reactions(
+            id=response_edit_message.data.id,
+            data=Postmessagereactions(code='👍')
+        )
+        response_add_reaction = await pachca.post_message_reactions(
+            id=response_edit_message.data.id,
+            data=Postmessagereactions(code='😱')
+        )
+        print('post_message_reactions', response_add_reaction, '\n','*'*60)
+        response_message_reactions = await pachca.get_message_reactions(
+            id=response_edit_message.data.id,
+        )
+        print('get_message_reactions', response_message_reactions, '\n','*'*60)
+        response_delete_reaction = await pachca.delete_message_reactions(
+            id=response_edit_message.data.id,
+            code='😱'
+        )
+        print('delete_message_reactions', response_delete_reaction, '\n','*'*60)
+        response_create_task = await pachca.create_task(
+            data=Createtask(
+                task={
+                    'kind': 'reminder',
+                    'content': 'дата в прошлом',
+                    'priority': 3,
+                    'due_at': '2025-12-24T18:00:29.000Z'
+                }
+            )
+        )
+        print('create_task', response_create_task, '\n','*'*60)
+        response = await pachca.leave_chat(
+            id=created_chat.data.id
+        )
+
+        print(response)
+
+        response_get_users = await pachca.get_employees()
+        print('get_employees', response_get_users, '\n','*'*60)
+        print(response_get_users.data[0].id)
+
+        response_get_user = await pachca.get_employee(
+            response_get_users.data[0].id
+        )
+        print('get_employee', response_get_user, '\n','*'*60)
+
+        response_put_status = await pachca.put_status(
+            data=Putstatus(
+                status={
+                    'emoji': '😱',
+                    'expires_at': '2025-12-24T17:47:25.000Z',
+                    'title': 'Статус из клиента АПИ'
+                }
+            )
+        )
+        print('put_status', response_put_status, '\n','*'*60)
+
+        response_get_status = await pachca.get_status()
+        print('get_status', response_get_status, '\n','*'*60)
+        
+        response_del_status = await pachca.del_status()
+        print('del_status', response_del_status, '\n','*'*60)
+
 
     asyncio.run(run_pachca())
 
-    print(type(pachca))
-    print(pachca.__class__)
-    print(pachca.__class__.__class__)
-    print(pachca.__class__.__class__.__class__)
+    # print(type(pachca))
+    # print(pachca.__class__)
+    # print(pachca.__class__.__class__)
+    # print(pachca.__class__.__class__.__class__)
