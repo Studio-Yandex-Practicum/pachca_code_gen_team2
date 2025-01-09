8097b293f60e6df6da1c7e2593f6be70e1d1a813
k0sdm1 <dmitrykostin968@gmail.com>
2025-01-09 18:37:26 +0300

    uft-8 for logger object

--- src/generator2/logger_setup.py
+++ src/generator2/logger_setup.py
@@ -4,11 +4,10 @@ from constants import LOG_FILE_NAME
 
 
 def setup_logging(logger_name: str) -> logging.Logger:
-    logging.basicConfig(encoding='utf-8')
     logger = logging.getLogger(logger_name)
     logger
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(LOG_FILE_NAME)
+    file_handler = logging.FileHandler(LOG_FILE_NAME, encoding='utf-8')
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
