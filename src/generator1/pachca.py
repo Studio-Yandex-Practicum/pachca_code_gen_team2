8a7e005835c672a06e86cb74cad557ea49a4f1d1
alex <alexmal24122004@gmail.com>
2025-01-09 21:23:33 +0300

    generate with new yaml

--- src/generator1/pachca.py
+++ src/generator1/pachca.py
@@ -1,12 +1,12 @@
 import asyncio
 
 from pachca_api_open_api_3_0_client.client import Pachca
+from pachca_api_open_api_3_0_client.models.chat import Chat
 from pachca_api_open_api_3_0_client.models.create_chat_body import (
     CreateChatBody,
 )
-from pachca_api_open_api_3_0_client.models.query_chat import QueryChat
 
-query_chat = QueryChat(name='test')
+query_chat = Chat(name='test')
 chat_body = CreateChatBody(chat=query_chat)
 
 
