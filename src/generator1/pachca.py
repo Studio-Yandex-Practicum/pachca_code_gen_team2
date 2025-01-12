cd33f6adb32d1e9dd3ebd96e4cb7e76acc619d1f
Владимир Кулаков <VrachKulakovVS@mail.ru>
2025-01-12 18:11:46 +0300

    env

--- src/generator1/pachca.py
+++ src/generator1/pachca.py
@@ -5,17 +5,7 @@ import os
 from dotenv import load_dotenv
 from logger_setup import setup_logging
 from pachca_api_open_api_3_0_client.client import Pachca
-from pachca_api_open_api_3_0_client.models import (
-    CreateTaskBodyTask,
-    EditMessageBody,
-    EditMessages,
-    QueryStatusStatus,
-)
-from pachca_api_open_api_3_0_client.models.base_chat import BaseChat
 from pachca_api_open_api_3_0_client.models.chat import Chat
-from pachca_api_open_api_3_0_client.models.code_reaction import (
-    CodeReaction,
-)
 from pachca_api_open_api_3_0_client.models.create_chat_body import (
     CreateChatBody,
 )
@@ -28,6 +18,16 @@ from pachca_api_open_api_3_0_client.models.create_messages import (
 from pachca_api_open_api_3_0_client.models.create_task_body import (
     CreateTaskBody,
 )
+
+from pachca_api_open_api_3_0_client.models import (
+    CreateTaskBodyTask, QueryStatusStatus, EditMessages, EditMessageBody
+)
+
+from pachca_api_open_api_3_0_client.models.base_chat import BaseChat
+from pachca_api_open_api_3_0_client.models.code_reaction import (
+    CodeReaction,
+)
+
 from pachca_api_open_api_3_0_client.models.put_status_body import (
     PutStatusBody,
 )
@@ -35,150 +35,159 @@ from pachca_api_open_api_3_0_client.models.put_status_body import (
 load_dotenv()
 pachca = Pachca(os.getenv('TOKEN'))
 
-logger = setup_logging(
-    'test_requests_logging',
-    'pachca_testresults.log'
-)
-
 
 async def main() -> None:
     """ Функция теста эндпоинтов """
 
+    # подготовка запроса на создание беседы -->
     query_chat = BaseChat(name='test500_2')
     chat_body = CreateChatBody(chat=query_chat)
-
+    # <--
+    # запрос на создание беседы
     chat_create = asyncio.create_task(
         pachca.createChat(body=chat_body))
-
     chat_response = await chat_create
+    print(chat_response)
+    print('*' * 60)
 
     # запрос на получение списка бесед и каналов
-
-    create_task = await asyncio.create_task(pachca.getChats())
+    print(await asyncio.create_task(pachca.getChats()))
+    print('*' * 60)
 
     # запрос на получение информации о беседе
-
-    getChat = await asyncio.create_task(
-        pachca.getChat(id=chat_response.data.id)
+    print(await asyncio.create_task(
+        pachca.getChat(id=chat_response.data.id))
     )
+    print('*' * 60)
 
     # подготовка запроса на создание сообщения в созданную беседу -->
-
     create_message = CreateMessages(
-
         entity_id=chat_response.data.id, content='Super puper')
-
     message_body = CreateMessageBody(message=create_message)
-
+    # <--
     # запрос на создание сообщения в созданную беседу
-
     message_create = asyncio.create_task(
         pachca.createMessage(body=message_body))
-
     message_response = await message_create
+    print(message_response)
+    print('*' * 60)
 
     # создание треда к созданному сообщению
-
     thread_create = asyncio.create_task(
         pachca.createThread(id=message_response.data.id)
     )
+    print(await thread_create)
+    print('*' * 60)
 
     # запрос на получение списка сообщений
-
-    getListMessage = await asyncio.create_task(
-
+    print(await asyncio.create_task(
         pachca.getListMessage(chat_id=chat_response.data.id))
+    )
+    print('*' * 60)
 
     # запрос на получение сообщения
-
-    getMessage = await asyncio.create_task(
-
+    print(await asyncio.create_task(
         pachca.getMessage(id=message_response.data.id))
+    )
+    print('*' * 60)
+    
 
     # подготовка запроса на редактирование сообщения -->
-
     edit_meassage = EditMessages(content='NOT SUPER PUPER')
-
     edit_message_body = EditMessageBody(message=edit_meassage)
-
+    # <--
     # запрос на редактирование сообщения
-
-    editMessage = await asyncio.create_task(
+    print(await asyncio.create_task(
         pachca.editMessage(
             id=message_response.data.id, body=edit_message_body)
         )
-
-    # запрос на удаление статуса
-
-    print(await asyncio.create_task(pachca.delStatus()))
-
-    # запрос на получение тегов сотрудников
-
-    print(await asyncio.create_task(pachca.getTags()))
-
-    # запрос на получение списка актуальных полей сущности
-
-    print(await asyncio.create_task(
-
-        pachca.getCommonMethods(entity_type='User'))
-
     )
+    print('*' * 60)
+    
+
+    # подготовка запроса на добавление реакции к сообщению -->
     post_reactions = CodeReaction(code='😭')
-    postMessageReactions = await pachca.postMessageReactions(
-        id=message_response.data.id, body=post_reactions
+    # <--
+    # запрос на добавление реакции к сообщению
+    print(await asyncio.create_task(
+        pachca.postMessageReactions(
+            id=message_response.data.id, body=post_reactions)
+        )
     )
+    print('*' * 60)
 
-    getMessageReactions = await pachca.getMessageReactions(id=message_response.data.id)
+    # подготовка запроса на получение списка реакций к сообщению -->
+    # <--
+    # запрос на получение списка реакций
+    print(await asyncio.create_task(
+        pachca.getMessageReactions(id=message_response.data.id))
+    )
+    print('*' * 60)
 
-    deleteMessageReactions = await pachca.deleteMessageReactions(id=message_response.data.id, code='😭')
+    # запрос на удаление реакции
+    print(await asyncio.create_task(
+        pachca.deleteMessageReactions(id=message_response.data.id, code='😭')
+        )
+    )
+    print('*' * 60)
 
-    # запрос на создание напоминания
-
+    # подготовка запроса на создание напоминания -->
     create_body_task = CreateTaskBodyTask(
         kind='call',
         content='Звонок другу',
         due_at=datetime.datetime.now(),
     )
-
     body_task = CreateTaskBody(task=create_body_task)
     # <--
+    # запрос на создание напоминания
+    print(await asyncio.create_task(pachca.createTask(body=body_task)))
+    print('*' * 60)
 
-    createtaskbody = await asyncio.create_task(pachca.createTask(body=body_task))
-
+    # запрос на получения списка пользователей
     users_response = await asyncio.create_task(pachca.getEmployees())
+    print(users_response)
+    print('*' * 60)
 
-    getEmployee = await asyncio.create_task(pachca.getEmployee(id=users_response.data[0].id))
+    # запрос на получение информации о пользователе
+    print(await asyncio.create_task(
+        pachca.getEmployee(id=users_response.data[0].id))
+    )
+    print('*' * 60)
+
+    # подготовка запроса на добавление статуса -->
+    query_status = QueryStatusStatus(
+        emoji='😭',
+        title='Я не плачу это просто слезы',
+        expires_at=None
+    )
+    # <--
+    # запрос на добавление статуса
+    print(await asyncio.create_task(pachca.putStatus(body=query_status)))
+    print('*' * 60)
+
+    # запрос на получение информации о своем статусе
+    print(await asyncio.create_task(pachca.getStatus()))
+    print('*' * 60)
+
+    
+
+    # запрос на удаление статуса
+    print(await asyncio.create_task(pachca.delStatus()))
+    print('*' * 60)
+
+    # запрос на получение тегов сотрудников
+    print(await asyncio.create_task(pachca.getTags()))
+    print('*' * 60)
+
+    # запрос на получение списка актуальных полей сущности
+    print(await asyncio.create_task(
+        pachca.getCommonMethods(entity_type='User'))
+    )
+    print('*' * 60)
 
     # запрос получения подписи и ключа для загрузки файла
-
-    logger.debug(await pachca.getUploads())
-
-    for task in (
-        chat_response,
-        create_task,
-        getChat,
-        message_response,
-        thread_create,
-        getListMessage,
-        getMessage,
-        editMessage,
-        create_task,
-        postMessageReactions,
-        getMessageReactions,
-        deleteMessageReactions,
-        createtaskbody,
-        users_response,
-        getEmployee,
-    ):
-
-        result = task
-
-        logger.debug(
-            f"{task}: data={result} \n"
-            "**",
-        )
-    logger.debug('Tests ended '+'*'*100)
-
+    print(await asyncio.create_task(pachca.getUploads()))
+    print('*' * 60)
 
 if __name__ == '__main__':
     asyncio.run(main())
