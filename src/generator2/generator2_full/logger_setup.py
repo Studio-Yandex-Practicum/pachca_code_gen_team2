232b8e0847029f81beb70c7eb8a3ae3876b812a3
k0sdm1 <dmitrykostin968@gmail.com>
2025-01-13 21:03:45 +0300

    place client log into generator2 directory

--- src/generator2/generator2_full/logger_setup.py
+++ src/generator2/generator2_full/logger_setup.py
@@ -1,14 +1,17 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
+from pathlib import Path
 
 from .constants import BACKUP_COUNT, LOG_FILE_NAME, MAX_FILE_SIZE
-
 
 def setup_logging(logger_name: str) -> logging.Logger:
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
     file_handler = RotatingFileHandler(
-        LOG_FILE_NAME, maxBytes=MAX_FILE_SIZE,
+        Path(os.path.dirname(os.path.abspath(
+            __file__))).parent / LOG_FILE_NAME,
+        maxBytes=MAX_FILE_SIZE,
         backupCount=BACKUP_COUNT,
         encoding='utf-8'
     )
